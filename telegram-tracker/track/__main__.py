d85bcc369017fa141d504515f401634d9af3cbd4
priya3242 <84270955+priya3242@users.noreply.github.com>
2023-11-26 14:34:17 +0530

    Add files via upload

---
+++ telegram-tracker/track/__main__.py
@@ -0,0 +1,73 @@
+
+from pytz import timezone 
+from datetime import datetime
+
+import pytz
+from settings import API_ID, API_HASH
+from sys import argv, exit
+from telethon import TelegramClient
+from telethon.tl.types import UserStatusOnline, UserStatusOffline
+from time import mktime, sleep
+
+import telethon.sync
+
+
+DATETIME_FORMAT = '%Y-%m-%d @ %H:%M:%S'
+client = TelegramClient('tracker', API_ID, API_HASH)
+
+
+def utc2localtime(utc):
+    pivot = mktime(utc.timetuple())
+    print(pivot)
+    offset = datetime.fromtimestamp(pivot) - datetime.utcfromtimestamp(pivot)
+    print(offset)
+    return utc + offset
+
+
+
+if len(argv) < 2:
+    print(f'usage: {argv[0]} <contact id>')
+    exit(1)
+contact_id = argv[1]
+
+client = TelegramClient('tracker', API_ID, API_HASH)
+client.start()
+
+online = None
+last_offline = None
+while True:
+    if contact_id in ['me', 'self']:
+        # Workaround for the regression in Telethon that breaks get_entity('me'):
+        # https://github.com/LonamiWebs/Telethon/issues/1024
+        contact = client.get_me()
+    else:
+        contact = client.get_entity(contact_id)
+
+    if isinstance(contact.status, UserStatusOffline):
+        if online != False:
+            online = False
+            ar = f'{utc2localtime(contact.status.was_online).strftime(DATETIME_FORMAT)}: User went offline.'
+            print(ar)
+            client.send_message('me', ar)
+        elif last_offline != contact.status.was_online:
+            if last_offline is not None:
+                ar = f'{utc2localtime(contact.status.was_online).strftime(DATETIME_FORMAT)}: User went offline after being online for short time.'
+                client.send_message('me', ar)
+            else:
+                ar = f'{utc2localtime(contact.status.was_online).strftime(DATETIME_FORMAT)}: User went offline.'
+                client.send_message('me', ar)
+        last_offline = contact.status.was_online
+    elif isinstance(contact.status, UserStatusOnline):
+        if online != True:
+            online = True
+            ar = f'~{datetime.now(timezone("Asia/Kolkata")).strftime(DATETIME_FORMAT)}: User went online.'
+            client.send_message('me', ar)
+    else:
+        if online != False:
+            online = False
+            ar = f'~{datetime.now(timezone("Asia/Kolkata")).strftime(DATETIME_FORMAT)}: User went offline.'
+            print(ar)
+            client.send_message('me', ar)
+        last_offline = None
+    sleep(15)
+
